3ec2bc99c8356af9d8af50e9af8d39149fe4fcff
wowhyuck <joohyuck.seo@gmail.com>
2023-08-01 05:10:40 +0900

    Root Bone 회전하기
    AO_Yaw의 범위(-90~90)에서 벗어나면 캐릭터가 회전하는 상태(Turning Left or Right)에 따라 캐릭터 회전하기

--- Source/Shooting/Character/ShooterCharacter.cpp
+++ Source/Shooting/Character/ShooterCharacter.cpp
@@ -141,6 +141,10 @@ void AShooterCharacter::AimOffset(float DeltaTime)
 		FRotator CurrentAimRotation = FRotator(0.f, GetBaseAimRotation().Yaw, 0.f);
 		FRotator DeltaAimRotation = UKismetMathLibrary::NormalizedDeltaRotator(StartingAimRotation, CurrentAimRotation);
 		AO_Yaw = DeltaAimRotation.Yaw;
+		if (TurningInPlace == ETurningInPlace::ETIP_NotTurning)
+		{
+			InterpAO_Yaw = AO_Yaw;
+		}
 		bUseControllerRotationYaw = true;
 		TurnInPlace(DeltaTime);
 	}
@@ -164,6 +168,16 @@ void AShooterCharacter::TurnInPlace(float DeltaTime)
 	else if (AO_Yaw < -90.f)
 	{
 		TurningInPlace = ETurningInPlace::ETIP_Left;
+	}
+	if (TurningInPlace != ETurningInPlace::ETIP_NotTurning)
+	{
+		InterpAO_Yaw = FMath::FInterpTo(InterpAO_Yaw, 0.f, DeltaTime, 4.f);
+		AO_Yaw = InterpAO_Yaw;
+		if (FMath::Abs(AO_Yaw) < 15.f)
+		{
+			TurningInPlace = ETurningInPlace::ETIP_NotTurning;
+			StartingAimRotation = FRotator(0.f, GetBaseAimRotation().Yaw, 0.f);
+		}
 	}
 }
 
--- Source/Shooting/Character/ShooterCharacter.h
+++ Source/Shooting/Character/ShooterCharacter.h
@@ -41,6 +41,7 @@ private:
 	class UCombatComponent* Combat;
 
 	float AO_Yaw;
+	float InterpAO_Yaw;
 	float AO_Pitch;
 	FRotator StartingAimRotation;
 
