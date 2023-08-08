803fe6d7a0d6857dca537746e0c94535fcc8a41d
wowhyuck <joohyuck.seo@gmail.com>
2023-08-09 03:42:00 +0900

    Projectile Tracer 만들기

--- Source/Shooting/Weapon/Projectile.cpp
+++ Source/Shooting/Weapon/Projectile.cpp
@@ -4,6 +4,9 @@
 #include "Projectile.h"
 #include "Components/BoxComponent.h"
 #include "GameFramework/ProjectileMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Particles/ParticleSystemComponent.h"
+#include "Particles/ParticleSystem.h"
 
 
 AProjectile::AProjectile()
@@ -25,7 +28,17 @@ AProjectile::AProjectile()
 void AProjectile::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	if (Tracer)
+	{
+		TracerComponent = UGameplayStatics::SpawnEmitterAttached(
+			Tracer,
+			CollisionBox,
+			FName(),
+			GetActorLocation(),
+			GetActorRotation(),
+			EAttachLocation::KeepWorldPosition);
+	}
 }
 
 void AProjectile::Tick(float DeltaTime)
