c10d6c0f6eb1e29a1115fc62b5d8521081ba077b
wowhyuck <joohyuck.seo@gmail.com>
2023-09-06 03:51:21 +0900

    무기 로켓런처 만들기
    로켓 탄약인 ProjectileRocket Class 만들기,
    ProjectileRocket과 Rocket Launcher 블루프린트 만들기

---
+++ Source/Shooting/Weapon/ProjectileRocket.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Projectile.h"
+#include "ProjectileRocket.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTING_API AProjectileRocket : public AProjectile
+{
+	GENERATED_BODY()
+	
+public:
+	AProjectileRocket();
+	
+protected:
+	virtual void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* RocketMesh;
+
+};
