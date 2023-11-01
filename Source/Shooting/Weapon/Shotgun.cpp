6ec6a7079205a8f9f37edfa7e994d48ee1d28253
wowhyuck <joohyuck.seo@gmail.com>
2023-11-02 04:14:34 +0900

    Shotgun 헤드샷 적용하기
    FireHit과 충돌된 적의 스켈레톤 부위가 head이면 헤드샷 데미지 적용하기
    1. 헤드샷을 맞춘 총알 개수와 적중된 적을 HeadShotHitMap에 저장
    2. 바디샷을 맞춘 총알 개수와 적중된 적을 HitMap에 저장
    3. DamageMap에 적중된 적과 전체 데미지(HeadDamage*헤드샷을 맞춘 총알 개수 + Damage*바디샷을 맞춘 총알 개수) 저장
    4. 맞춘 적에게 데미지 적용

--- Source/Shooting/Weapon/Shotgun.cpp
+++ Source/Shooting/Weapon/Shotgun.cpp
@@ -26,19 +26,28 @@ void AShotgun::Fire(const FVector& HitTarget)
 		FVector Start = SocketTransform.GetLocation();
 
 		TMap<AEnemy*, uint32> HitMap;
+		TMap<AEnemy*, uint32> HeadShotHitMap;
+
 		for (uint32 i = 0; i < NumberOfPellets; i++)
 		{
 			FHitResult FireHit;
 			WeaponTraceHit(Start, HitTarget, FireHit);
 
 			AEnemy* HitEnemy = Cast<AEnemy>(FireHit.GetActor());
-			if (HitMap.Contains(HitEnemy))
+
+			const bool bHeadShot = FireHit.BoneName.ToString() == FString("head");
+
+			// 헤드샷일 때, 맞춘 적(Key)과 맞은 총알 개수(Value)를 HeadSHotHitMap에 저장하기
+			if (bHeadShot)
 			{
-				HitMap[HitEnemy]++;
+				if (HeadShotHitMap.Contains(HitEnemy)) HeadShotHitMap[HitEnemy]++;
+				else HeadShotHitMap.Emplace(HitEnemy, 1);
 			}
+			// 헤드샷이 아닐 때(바디샷일 때), 맞춘 적(Key)과 맞은 총알 개수(Value)를 HitMap에 저장하기
 			else
 			{
-				HitMap.Emplace(HitEnemy, 1);
+				if (HitMap.Contains(HitEnemy)) HitMap[HitEnemy]++;
+				else HitMap.Emplace(HitEnemy, 1);
 			}
 
 			if (ImpactParticles)
@@ -61,13 +70,42 @@ void AShotgun::Fire(const FVector& HitTarget)
 			}
 		}
 
+		TArray<AEnemy*> HitEnemies;
+		TMap<AEnemy*, float> DamageMap;
+
+		// 바디샷일 때 저장된 HitMap의 맞춘 적(Key)과 적에게 적용된 데미지(Value, 맞은 총알 개수 * 총알 한 발당 데미지)를
+		// DamageMap에 저장하기
 		for (auto HitPair : HitMap)
 		{
 			if (HitPair.Key && InstigatorController)
 			{
+				DamageMap.Emplace(HitPair.Key, HitPair.Value * Damage);
+
+				HitEnemies.AddUnique(HitPair.Key);
+			}
+		}
+
+		// 헤드샷일 때 저장된 HitMap의 맞춘 적(Key)과 적에게 적용된 데미지(Value, 맞은 총알 개수 * 총알 한 발당 헤드데미지)를
+		// DamageMap의 저장된 데미지에 더해서 저장하기
+		for (auto HeadShotHitPair : HeadShotHitMap)
+		{
+			if (HeadShotHitPair.Key)
+			{
+				if (DamageMap.Contains(HeadShotHitPair.Key)) DamageMap[HeadShotHitPair.Key] += HeadShotHitPair.Value * HeadDamage;
+				else DamageMap.Emplace(HeadShotHitPair.Key, HeadShotHitPair.Value * HeadDamage);
+
+				HitEnemies.AddUnique(HeadShotHitPair.Key);
+			}
+		}
+
+		// 맞춘 적(Key)에게 데미지(Value)를 적용하기
+		for (auto DamagePair : DamageMap)
+		{
+			if (DamagePair.Key && InstigatorController)
+			{
 				UGameplayStatics::ApplyDamage(
-					HitPair.Key,
-					Damage * HitPair.Value,
+					DamagePair.Key,
+					DamagePair.Value,
 					InstigatorController,
 					this,
 					UDamageType::StaticClass());
