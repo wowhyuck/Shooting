3f0883be6a98ee0f9beedc1cfa9cbeec12ef8a26
wowhyuck <joohyuck.seo@gmail.com>
2023-11-08 23:50:04 +0900

    EnemyController(AI Controller) 파일 만들기

---
+++ Source/Shooting/Enemy/EnemyController.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "EnemyController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTING_API AEnemyController : public AAIController
+{
+	GENERATED_BODY()
+	
+public:
+	AEnemyController();
+	virtual void OnPossess(APawn* InPawn) override;
+
+	UPROPERTY(BlueprintReadWrite)
+	class UBlackboardComponent* BlackboardComponent;
+
+	UPROPERTY(BlueprintReadWrite)
+	class UBehaviorTreeComponent* BehaviorTreeComponent;
+
+private:
+
+public:
+	FORCEINLINE UBlackboardComponent* GetBlackboardComponent() const { return BlackboardComponent; }
+};
