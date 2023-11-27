638b49d59adab684890586ace4303a20393fe9c0
wowhyuck <joohyuck.seo@gmail.com>
2023-11-28 05:49:48 +0900

    Buff Component 만들기

---
+++ Source/Shooting/ShooterComponents/BuffComponent.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "BuffComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class SHOOTING_API UBuffComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UBuffComponent();
+	friend class AShooterCharacter;
+	//void Heal(float HealAmount);
+	//void TakeGold(int32 GoldAmount);
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY()
+	class AShooterCharacter* Character;
+
+public:
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+};
